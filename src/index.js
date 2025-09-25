82b3cecf821f34c386f2b663f19d0746fbbef007
astkalich <92906800+astkalich@users.noreply.github.com>
2025-09-25 11:06:00 +0000

    number point

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ function func_for_integrate(x) {
     return 4 * x - x * x;
 }
 
-function Integrate(a, b, f, l) {
+function Integrate(a, b, f, n) {
+    var l = (b-a) / n;
     var sum = 0;
     for (var i = a + 0.5 * l; i < b; i += l) {
         sum += f(i) * l;
@@ -12,5 +13,5 @@ function Integrate(a, b, f, l) {
 
 var a = Number(prompt('a:'));
 var b = Number(prompt('b:'));
-var func = Integrate(a, b, func_for_integrate, 0.001);
-alert('' + func);
+var func = Integrate(a, b, func_for_integrate, 100);
+alert(func);
