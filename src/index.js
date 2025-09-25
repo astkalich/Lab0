94124223cdd795b3fd5e1fe5492d4f36d71afac9
astkalich <92906800+astkalich@users.noreply.github.com>
2025-09-25 09:03:18 +0000

    solve

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("Let's integrate!")
+function func_for_integrate(x) {
+    return 4 * x - x * x;
+}
+
+function Integrate(a, b, f, l) {
+    var sum = 0;
+    for (var i = a + 0.5 * l; i < b; i += l) {
+        sum += f(i) * l;
+    }
+    return sum;
+}
+
+var a = Number(prompt('a:'));
+var b = Number(prompt('b:'));
+var func = Integrate(a, b, func_for_integrate, 0.001);
+alert('' + func);
